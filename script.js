788c5c8ce8e3c1d2f0221838b14309a22e22079d
NIRAJ DHANDHUKIYA <147057155+NIRAJDHANDHUKIYA@users.noreply.github.com>
2024-06-17 00:05:26 +0530

    comment wide & no ball script.js

--- script.js
+++ script.js
@@ -50,12 +50,12 @@ function addExtraBall(type) {
     const ballsContainer = document.getElementById("balls-container");
     const ball = document.createElement("div");
     ball.classList.add("ball");
-    if (type === "wide" || type === "no") {
-      ball.innerText = type === "wide" ? "WD1" : "NB1";
-      currentOverScore += 1;
+//    if (type === "wide" || type === "no") {
+  //    ball.innerText = type === "wide" ? "WD1" : "NB1";
+  //    currentOverScore += 1;
       // Update current score only for extras
-      currentScore += 1;
-    }
+    //  currentScore += 1;
+   // }
     ballsContainer.appendChild(ball);
 
     // Update the current team's score immediately
